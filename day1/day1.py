c5e6c321d8f58361c232eb350a6c5908490a2673
risboo65909 <ttyv00@gmail.com>
2021-12-01 10:50:32 +0300

    aoc2021: day 1 soultion

---
+++ day1/day1.py
@@ -0,0 +1,44 @@
+import os
+
+
+def silver(lines):
+    prev_depth = None
+    result = 0
+
+    for line in lines:
+        depth = int(line)
+        if prev_depth is not None:
+            if depth > prev_depth:
+                result += 1
+        prev_depth = depth
+
+    return result
+
+
+def gold(lines):
+    prev_sum_depth = None
+    result = 0
+    window = []
+
+    for line in lines:
+        depth = int(line)
+        window.append(depth)
+
+        if len(window) == 3:
+
+            sum_depth = sum(window)
+            if prev_sum_depth != None and sum_depth > prev_sum_depth:
+                result += 1
+
+            prev_sum_depth = sum_depth
+            window = window[1:]
+
+    return result
+
+
+def solve():
+    lines = open(os.path.join(os.path.dirname(
+        __file__), 'input'), 'rt').readlines()
+
+    print(silver(lines))
+    print(gold(lines))
---
+++ run_all.py
@@ -0,0 +1,4 @@
+import day1.day1 as d1
+
+if __name__ == '__main__':
+    d1.solve()
