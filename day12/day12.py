55d53f8fbe3d9568fc20300117ba9dd3b74b44e9
risboo65909 <ttyv00@gmail.com>
2021-12-12 15:52:01 +0300

    day12

---
+++ day12/day12.py
@@ -0,0 +1,55 @@
+import os
+from collections import defaultdict
+
+
+def dfs(nodes, cur_node, freq, twice):
+    if cur_node == "end":
+        return 1
+
+    if cur_node.islower():
+
+        if freq[cur_node] > 0:
+            if not twice:
+                # can't visit small caves more than once
+                return 0
+            elif cur_node == "start" or any(map(lambda v: v == 2, freq.values())):
+                return 0
+
+        freq[cur_node] += 1
+
+    total_paths = 0
+
+    for dest_node in nodes[cur_node]:
+        total_paths += dfs(nodes, dest_node, freq, twice)
+
+    if cur_node.islower() and freq[cur_node] > 0:
+        freq[cur_node] -= 1
+
+    return total_paths
+
+
+def silver(nodes):
+    return dfs(nodes, "start", defaultdict(int), twice=False)
+
+
+def gold(nodes):
+    return dfs(nodes, "start", defaultdict(int), twice=True)
+
+
+def parse(lines):
+    nodes = defaultdict(list)
+
+    for line in lines:
+        from_node, to_node = line.strip().split("-")
+        nodes[from_node].append(to_node)
+        nodes[to_node].append(from_node)
+
+    return nodes
+
+
+def solve():
+    lines = open(os.path.join(os.path.dirname(__file__), "input"), "rt").readlines()
+
+    nodes = parse(lines)
+
+    return "DAY 12", silver(nodes), gold(nodes)
--- run_all.py
+++ run_all.py
@@ -8,6 +8,8 @@ import day7.day7 as d7
 import day8.day8 as d8
 import day9.day9 as d9
 import day10.day10 as d10
+import day11.day11 as d11
+import day12.day12 as d12
 
 
 def show_solution(solution_tuple):
@@ -16,6 +18,6 @@ def show_solution(solution_tuple):
 
 
 if __name__ == "__main__":
-    problems = [d1, d2, d3, d4, d5, d6, d7, d8, d9, d10]
+    problems = [d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12]
     for problem in problems:
         show_solution(problem.solve())
