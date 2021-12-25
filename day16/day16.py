a0048b66cd6669fefea7d034669ba0905d1bf3a2
Boris Tatarintsev <bktatarintsev@avito.ru>
2021-12-25 13:45:28 +0300

    day 16 wip

--- day16/day16.py
+++ day16/day16.py
@@ -1,11 +1,20 @@
 import os
+from functools import reduce
 
 
-def silver(parsed):
-    pass
+def silver(tokens):
+    net = 0
+    for token in tokens:
+        if isinstance(token, list):
+            net += silver(token)
+        else:
+            net += token.version
+        
+    return net
 
 
-def gold(parsed):
+def gold(tokens):
+    
     pass
 
 
@@ -13,6 +22,9 @@ class Literal(object):
 
     PACKET_ID = 4
     GROUP_SIZE = 5
+
+    def __init__(self, version):
+        self.version = version
 
     def parse(self, bits):
         try:
@@ -34,39 +46,55 @@ class Literal(object):
 class SubPacket(object):
 
     PACKET_ID = 6
-    HEADER_SIZE = 16
+
+    BITS_LEN = 16
+    BITS_PACKETS = 12
+
+    def __init__(self, version, type_id):
+        self.version = version
+        self.type_id = type_id
+
+    def apply_op(self):
+        pass
 
     def parse(self, bits):
 
         ABSOLUTE_LEN_ID = 0
         NUMBER_OF_SUBPACKETS_ID = 1
 
-        length_type_id = int(bits[0])
+        length_type_id = int(bits[0], 2)
 
         try:
-
-            num = bits[1: SubPacket.HEADER_SIZE]
-            num = int(''.join(num), 2)
 
             cur_idx = 0
             sub_packets = []
 
             if length_type_id == ABSOLUTE_LEN_ID:
-                num_of_bits = num
+
+                num_of_bits = bits[1: SubPacket.BITS_LEN]
+                num_of_bits = int(''.join(num_of_bits), 2)
 
                 while cur_idx < num_of_bits:
-                    parsed, delta = parse_bits(bits[16+cur_idx:16+num_of_bits])
+                    parsed, delta = parse_bits(bits[SubPacket.BITS_LEN+cur_idx: SubPacket.BITS_LEN+num_of_bits])
                     sub_packets.extend(parsed)
                     cur_idx += delta
 
-                return sub_packets, cur_idx + SubPacket.HEADER_SIZE
-            else:
-                num_of_packets = num
+                return sub_packets, cur_idx + SubPacket.BITS_LEN
+
+            elif length_type_id == NUMBER_OF_SUBPACKETS_ID:
+
+                num_of_packets = bits[1: SubPacket.BITS_PACKETS]
+                num_of_packets = int(''.join(num_of_packets), 2)
+
+                for _ in range(num_of_packets):
+                    parsed, delta = parse_bits(bits[SubPacket.BITS_PACKETS+cur_idx:])
+                    sub_packets.extend(parsed)
+                    cur_idx += delta
+
+                return sub_packets, cur_idx + SubPacket.BITS_PACKETS
 
         except:
             return [], 0
-        else:
-            return sub_packets, cur_idx + SubPacket.HEADER_SIZE
 
 
 def parse_bits(bits):
@@ -81,15 +109,22 @@ def parse_bits(bits):
         # skip first 6 bits of version and type_id
         cur_idx += 6
 
-        if int(type_id, 2) == Literal.PACKET_ID:
-            literal = Literal()
+        version = int(version, 2)
+        type_id = int(type_id, 2)
+
+        if type_id == Literal.PACKET_ID:
+            literal = Literal(version)
             cur_idx += literal.parse(bits[cur_idx:])
+
             parsed.append(literal)
+
         else:
-            sub_packet = SubPacket()
+            sub_packet = SubPacket(version, type_id)
             sub_parsed, delta = sub_packet.parse(bits[cur_idx:])
             cur_idx += delta
-            parsed.extend(sub_parsed)
+
+            parsed.append([sub_packet, [*sub_parsed]])
+            #parsed.extend(sub_parsed)
 
     return parsed, cur_idx
 
@@ -97,15 +132,16 @@ def parse_bits(bits):
 def parse(lines):
     hex_str = lines[0]
     bits = bin(int('1'+hex_str, 16))[3:]
-    parsed, _ = parse_bits(bits)
+    tokens, _ = parse_bits(bits)
 
-    for token in parsed:
-        print(token)
+    return tokens
 
 
 def solve():
     lines = open(os.path.join(os.path.dirname(
         __file__), "input"), "rt").readlines()
-    parsed = parse(lines)
+    tokens = parse(lines)
 
-    return "DAY16", silver(parsed), gold(parsed)
+    #print(tokens)
+
+    return "DAY16", silver(tokens), gold(tokens)
