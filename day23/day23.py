24fb0ed1c26232daa2c0732d7831dbecb8ba0806
risboo65909 <ttyv00@gmail.com>
2022-03-30 12:03:21 +0300

    day 23 made parallel + black

--- day23/day23.py
+++ day23/day23.py
@@ -1,27 +1,28 @@
 import os
+from multiprocessing import Pool
 
 
 energy = {
-    'A': 1,
-    'B': 10,
-    'C': 100,
-    'D': 1000,
+    "A": 1,
+    "B": 10,
+    "C": 100,
+    "D": 1000,
 }
 
 
 def get(xs, i):
     if len(xs) > i:
         return xs[i]
-    return '.'
+    return "."
 
 
 class Board(object):
 
     room_indices = {
-        'A': 3,
-        'B': 5,
-        'C': 7,
-        'D': 9,
+        "A": 3,
+        "B": 5,
+        "C": 7,
+        "D": 9,
     }
 
     def __init__(self, hall, rooms, room_size):
@@ -30,27 +31,46 @@ class Board(object):
         self.room_size = room_size
 
     def __str__(self):
-        res = '#'*len(self.hall)+'\n'
-        res += ''.join((s for s in self.hall))+'\n'
+        res = "#" * len(self.hall) + "\n"
+        res += "".join((s for s in self.hall)) + "\n"
 
-        res += ''.join((get(self.rooms[i], self.room_size-1)
-                       if i in self.rooms else '#' for i in range(13)))+'\n'
+        res += (
+            "".join(
+                (
+                    get(self.rooms[i], self.room_size - 1) if i in self.rooms else "#"
+                    for i in range(13)
+                )
+            )
+            + "\n"
+        )
 
-        for k in range(self.room_size-2, -1, -1):
-            res += '  #'+''.join((get(self.rooms[i], k)
-                                  if i in self.rooms else '#' for i in range(3, 10))) + '#  \n'
-        res += '  #########  '
+        for k in range(self.room_size - 2, -1, -1):
+            res += (
+                "  #"
+                + "".join(
+                    (
+                        get(self.rooms[i], k) if i in self.rooms else "#"
+                        for i in range(3, 10)
+                    )
+                )
+                + "#  \n"
+            )
+        res += "  #########  "
 
         return res
 
     def __hash__(self):
-        return hash((tuple(self.hall), tuple((k, tuple(v)) for k, v in self.rooms.items())))
+        return hash(
+            (tuple(self.hall), tuple((k, tuple(v)) for k, v in self.rooms.items()))
+        )
 
     def __eq__(self, other):
         return self.hall == other.hall and self.rooms == other.rooms
 
     def clone(self):
-        return Board(self.hall[:], {k: v[:] for k, v in self.rooms.items()}, self.room_size)
+        return Board(
+            self.hall[:], {k: v[:] for k, v in self.rooms.items()}, self.room_size
+        )
 
     def is_proper_room(self, req_room_idx, to_room_idx):
         if req_room_idx != to_room_idx:
@@ -63,19 +83,19 @@ class Board(object):
 
     def has_obstacle(self, from_idx, to_idx):
         if to_idx >= from_idx:
-            for i in range(from_idx+1, to_idx+1):
-                if self.hall[i] != '.':
+            for i in range(from_idx + 1, to_idx + 1):
+                if self.hall[i] != ".":
                     return True
         else:
             for i in range(to_idx, from_idx):
-                if self.hall[i] != '.':
+                if self.hall[i] != ".":
                     return True
 
         return False
 
     # pops item from room_idx and place it to dest_idx in the hall
     def pop_to_hall(self, room_idx, to_idx):
-        if self.hall[to_idx] != '.':
+        if self.hall[to_idx] != ".":
             return False
 
         if len(self.rooms[room_idx]) == 0:
@@ -87,11 +107,13 @@ class Board(object):
         item = self.rooms[room_idx].pop()
         self.hall[to_idx] = item
 
-        return energy[item] * (abs(room_idx-to_idx) + self.room_size-len(self.rooms[room_idx]))
+        return energy[item] * (
+            abs(room_idx - to_idx) + self.room_size - len(self.rooms[room_idx])
+        )
 
     # get item from the hall and move it into the room
     def push_from_hall(self, from_idx, to_room_idx):
-        if self.hall[from_idx] == '.':
+        if self.hall[from_idx] == ".":
             return False
 
         if len(self.rooms[to_room_idx]) == self.room_size:
@@ -110,9 +132,14 @@ class Board(object):
 
         item = self.hall[from_idx]
         self.rooms[to_room_idx].append(item)
-        self.hall[from_idx] = '.'
+        self.hall[from_idx] = "."
 
-        return energy[item] * (abs(from_idx-to_room_idx) + self.room_size-len(self.rooms[to_room_idx])+1)
+        return energy[item] * (
+            abs(from_idx - to_room_idx)
+            + self.room_size
+            - len(self.rooms[to_room_idx])
+            + 1
+        )
 
     def is_done(self):
         for label, room_idx in Board.room_indices.items():
@@ -128,7 +155,7 @@ def solver(board, max_depth):
 
     visited = {}
 
-    min_so_far = [float('inf')]
+    min_so_far = [float("inf")]
 
     hall_range = [1, 2, 4, 6, 8, 10, 11]
     room_indices = Board.room_indices.values()
@@ -156,7 +183,7 @@ def solver(board, max_depth):
             for room_idx in room_indices:
                 energy_used = board.push_from_hall(from_idx, room_idx)
                 if energy_used:
-                    rec(board, total_energy + energy_used, depth+1)
+                    rec(board, total_energy + energy_used, depth + 1)
                     board = cloned_board.clone()
 
         # pop from room to hall
@@ -165,7 +192,7 @@ def solver(board, max_depth):
             for to_idx in hall_range:
                 energy_used = board.pop_to_hall(room_idx, to_idx)
                 if energy_used:
-                    rec(board, total_energy + energy_used, depth+1)
+                    rec(board, total_energy + energy_used, depth + 1)
                     board = cloned_board.clone()
 
     rec(board, 0, 0)
@@ -174,22 +201,25 @@ def solver(board, max_depth):
 
 
 def silver(board):
-    return solver(board, 16)
+    return "silver", solver(board, 16)
 
 
 def gold(board):
-    return solver(board, 32)
+    return "gold", solver(board, 32)
 
 
 def parse(lines, room_size):
     hall = list(lines[1].strip())
 
-    room_indices = (i for i, s in enumerate(lines[2].strip()) if s != '#')
+    room_indices = (i for i, s in enumerate(lines[2].strip()) if s != "#")
 
-    rooms = [[s] if s != '.' else [] for s in lines[2].strip() if s != '#']
-    for k in range(room_size-1):
-        rooms = [[s] + rooms[i//2]
-                 for i, s in enumerate(lines[3+k].strip()) if (i % 2) != 0]
+    rooms = [[s] if s != "." else [] for s in lines[2].strip() if s != "#"]
+    for k in range(room_size - 1):
+        rooms = [
+            [s] + rooms[i // 2]
+            for i, s in enumerate(lines[3 + k].strip())
+            if (i % 2) != 0
+        ]
 
     indices_2_rooms = {}
     for idx, room in zip(room_indices, rooms):
@@ -201,15 +231,27 @@ def parse(lines, room_size):
 
 
 def solve():
-    lines_silver = open(os.path.join(os.path.dirname(
-        __file__), "input_silver"), "rt").readlines()
+    p = Pool(2)
+
+    lines_silver = open(
+        os.path.join(os.path.dirname(__file__), "input_silver"), "rt"
+    ).readlines()
     board_silver = parse(lines_silver, room_size=2)
 
-    lines_gold = open(os.path.join(os.path.dirname(
-        __file__), "input_gold"), "rt").readlines()
+    lines_gold = open(
+        os.path.join(os.path.dirname(__file__), "input_gold"), "rt"
+    ).readlines()
     board_gold = parse(lines_gold, room_size=4)
 
-    result_silver = silver(board_silver)
-    result_gold = gold(board_gold)
+    with Pool(processes=2) as p:
+        res = dict(
+            [
+                worker.get()
+                for worker in [
+                    p.apply_async(silver, (board_silver,)),
+                    p.apply_async(gold, (board_gold,)),
+                ]
+            ]
+        )
 
-    return "DAY23", result_silver, result_gold
+    return "DAY23", res["silver"], res["gold"]
--- day23/tests.py
+++ day23/tests.py
@@ -11,13 +11,12 @@ input = """
 
 
 class TestBoardMethods(unittest.TestCase):
-
     def setUp(self):
         self.board = parse(input.split(), 2)
 
     def test_pop_to_hall(self):
         assert self.board.pop_to_hall(7, 4) == 40
 
-        assert self.board.hall[4] == 'B'
-        assert self.board.rooms[7][0] == 'C'
+        assert self.board.hall[4] == "B"
+        assert self.board.rooms[7][0] == "C"
         assert len(self.board.rooms[7]) == 1
