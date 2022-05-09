3376be09f559029184631256168464e236770ef7
Boris Tatarintsev <risboo6909@MacBook-Air-Boris.local>
2022-05-09 22:07:52 +0300

    Use GA to analyze day 24 soltion magnitude

--- day24/alu.py
+++ day24/alu.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 """
 The ALU is a four-dimensional processing unit: it has integer variables w, x, y, and z. These variables all start with the value 0. The ALU also supports six instructions:
 
@@ -13,23 +15,138 @@ eql a b - If the value of a and b are equal, then store the value 1 in variable 
 registers = {'w', 'x', 'y', 'z'}
 
 
+def is_number(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
 class OpInp(object):
-    pass
+    def __init__(self, reg_data, inp_stream, dst):
+        assert dst in registers
+        self.reg_data = reg_data
+        self.inp_stream = inp_stream
+        self.dst = dst
+
+    def compute(self):
+        assert len(self.inp_stream) > 0
+        val = self.inp_stream.pop(0)
+        self.reg_data[self.dst] = int(val)
 
 
 class OpAdd(object):
-    def __init__(self, a, b):
-        pass
+    def __init__(self, reg_data, dst, b):
+        assert dst in registers
+        assert b in registers or is_number(b)
+        self.reg_data = reg_data
+        self.dst = dst
+        self.b = b
+
+    def compute(self):
+        if is_number(self.b):
+            self.reg_data[self.dst] += int(self.b)
+        else:
+            self.reg_data[self.dst] += self.reg_data[self.b]
+
+class OpMul(object):
+    def __init__(self, reg_data, dst, b):
+        assert dst in registers
+        assert b in registers or is_number(b)
+        self.reg_data = reg_data
+        self.dst = dst
+        self.b = b
+    
+    def compute(self):
+        if is_number(self.b):
+            self.reg_data[self.dst] *= int(self.b)
+        else:
+            self.reg_data[self.dst] *= self.reg_data[self.b]
+
+class OpDiv(object):
+    def __init__(self, reg_data, dst, b):
+        assert dst in registers
+        assert b in registers or is_number(b)
+        self.reg_data = reg_data
+        self.dst = dst
+        self.b = b
+
+    def compute(self):
+        if is_number(self.b):
+            self.reg_data[self.dst] /= int(self.b)
+        else:
+            self.reg_data[self.dst] /= self.reg_data[self.b]
+
+        self.reg_data[self.dst] = int(self.reg_data[self.dst])
+
+class OpMod(object):
+    def __init__(self, reg_data, dst, b):
+        assert dst in registers
+        assert b in registers or is_number(b)
+        self.reg_data = reg_data
+        self.dst = dst
+        self.b = b
+
+    def compute(self):
+        if is_number(self.b):
+            self.reg_data[self.dst] %= int(self.b)
+        else:
+            self.reg_data[self.dst] %= self.reg_data[self.b]
+
+class OpEql(object):
+    def __init__(self, reg_data, dst, b):
+        assert dst in registers
+        assert b in registers or is_number(b)
+        self.reg_data = reg_data
+        self.dst = dst
+        self.b = b
+
+    def compute(self):
+        if is_number(self.b):
+            self.reg_data[self.dst] = int(self.reg_data[self.dst] == int(self.b))
+        else:
+            self.reg_data[self.dst] = int(self.reg_data[self.dst] == self.reg_data[self.b])
 
 
 class ALU(object):
 
     def __init__(self):
         self.input_stream = []
-        self.reg_data = {}
+        self.program = []
+        self.reg_data = defaultdict(int)
+        
+    def reset(self):
+        self.input_stream.clear()
+        self.reg_data.clear()
 
+    def choose_op(self, op_raw):
+        op_name, args = op_raw.strip().split(' ', maxsplit=1)
+        if len(args.split()) == 1:
+            # only input can have one argument all the others have two
+            return OpInp(self.reg_data, self.input_stream, args)
+
+        arg1, arg2 = args.split()
+        if op_name == 'add':
+            return OpAdd(self.reg_data, arg1, arg2)
+        elif op_name == 'mul':
+            return OpMul(self.reg_data, arg1, arg2)
+        elif op_name == 'div':
+            return OpDiv(self.reg_data, arg1, arg2)
+        elif op_name == 'mod':
+            return OpMod(self.reg_data, arg1, arg2)
+        elif op_name == 'eql':
+            return OpEql(self.reg_data, arg1, arg2)
+
+        assert False, 'Operator not recognized: {}'.format(op_raw)
+ 
     def parse_program(self, lines):
-        pass
+        for line in lines:
+            op = self.choose_op(line)
+            self.program.append(op)
 
-    def input_stream(self, input_stream):
-        pass
+    def run_program(self):
+        for op in self.program:
+            op.compute()
+
+    def set_input_stream(self, *input_stream):
+        self.input_stream.extend(input_stream)
--- day24/day24.py
+++ day24/day24.py
@@ -1,23 +1,115 @@
 import os
+import random
+from collections import defaultdict
+from pyeasyga import pyeasyga
+from .alu import ALU
 
+#INF = 99999999999999
 
-def silver(parsed):
+numbers = [k for k in range(1, 10) for x in range(k)]
+
+def choose_number():
+    return random.choice(numbers)
+
+def silver_ga(alu, best):
+
+    def create_individual(data):
+        #return [choose_number() for _ in range(14)]
+        individual = data[:]
+        random.shuffle(individual)
+        return individual
+
+    def fitness(individual, _):
+        alu.reset()
+        alu.set_input_stream(*individual)
+        alu.run_program()
+
+        penalty = 0 #10.0 / int(''.join(map(str, individual)))
+        # if individual[0] < 9:
+        #     penalty += (9 - individual[0])
+        # if individual[1] < 9:
+        #     penalty += (9 - individual[1])
+
+        if alu.reg_data['z'] == 0:
+            return 0
+        
+        return alu.reg_data['z'] + penalty
+
+    def mutate(individual):
+        mutate_index1 = random.randrange(len(individual))
+        individual[mutate_index1] = choose_number()
+        
+    def selection(population):
+        return random.choice(population)
+
+    ga = pyeasyga.GeneticAlgorithm(best, 
+    generations=1000, 
+    population_size=50, 
+    crossover_probability=0.3, 
+    mutation_probability=0.4,
+    elitism=True,
+    maximise_fitness=False)
+
+    ga.create_individual = create_individual
+    ga.fitness_function = fitness
+    ga.mutate_function = mutate
+    ga.selection_function = selection
+    ga.run()
+    
+    return ga.best_individual()[1]
+
+def silver(alu):
+    best = [choose_number() for _ in range(14)]
+    best = [9, 6, 9, 7, 9, 6, 5, 9, 6, 9, 2, 4, 9, 5]
+    best = [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]
+    while True:
+        best = silver_ga(alu, best)
+
+        alu.reset()
+        alu.set_input_stream(*list(map(int, best)))
+        alu.run_program()
+        
+        print(best, alu.reg_data['z'])
+        if alu.reg_data['z'] == 0:
+            break
+
+    # periods = defaultdict(int)
+    # last_seen = {}
+
+    # for n in range (99999999999999, 11111111111110, -1):
+    #     alu.reset()
+
+    #     alu.set_input_stream(*list(map(int, str(n))))
+    #     alu.run_program()
+
+    #     res = alu.reg_data['z']
+    #     res_str = str(res)
+    #     res_len = len(res_str)
+
+    #     if res_len in last_seen:
+    #         periods[res_len] = last_seen[res_len] - n
+
+    #     last_seen[res_len] = n
+
+    #     print(periods, n, res)
+
+    #     #print(n, res)
+    #     if res == 0:
+    #         return n
+
+    # return False
+
+def gold(alu):
     pass
-
-
-def gold(parsed):
-    pass
-
 
 def parse(lines):
-    parsed = lines
-    return parsed
+    alu = ALU()
+    alu.parse_program(lines)
+    return alu
 
 
 def solve():
     lines = open(os.path.join(os.path.dirname(
         __file__), "input"), "rt").readlines()
 
-    parsed = parse(lines)
-
-    return "DAY24", silver(parsed), gold(parsed)
+    return "DAY24", silver(parse(lines)), 1 #gold(parse(lines))
